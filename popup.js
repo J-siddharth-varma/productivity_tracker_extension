0b21d79db1df10c1c83071d36862b5c078d1b5c4
siddharthvarmaj <siddharthvarmaj@gmail.com>
2024-09-06 17:33:14 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -34,7 +34,7 @@ function updateTimerDisplay() {
 }
 
 function formatTime(seconds) {
-    const hours = Math.floor(seconds / 3600);
+     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
     return [hours, minutes, secs]
